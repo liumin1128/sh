88bfb557ad3a486d72faf08b6ccf5084b7d568a7
liumin1128 <970568830@qq.com>
2018-02-02 14:27:23 +0800

    更新

--- centos.sh
+++ centos.sh
@@ -6,7 +6,7 @@ yum update kernel
 echo '更新软件仓库'
 yum update && yum upgrade 
 
-echo '安装初始软件'
+echo '安装软件 zsh git wget'
 yum -y install zsh git wget
 
 source ~/.zshrc
@@ -14,7 +14,7 @@ source ~/.zshrc
 echo '安装o-my-zsh'
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-# echo 'nvm'
-# wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+echo 'nvm'
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | zsh
 
 # echo "Intel Galileo" >> test.txt  
