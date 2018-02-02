d0813226b7b607d33da15c87d9970acc359d7b05
liumin1128 <970568830@qq.com>
2018-02-02 14:44:51 +0800

    更新

--- centos.sh
+++ centos.sh
@@ -18,17 +18,19 @@ echo 'nvm'
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | zsh
 
 echo '将nvm加入zshrc'
-echo '\nexport NVM_DIR="$HOME/.nvm"
+echo '
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.zshrc && source ~/.zshrc
 
-# echo '\n#alias for cnpm\nalias cnpm="npm --registry=https://registry.npm.taobao.org \
+# echo #alias for cnpm\nalias cnpm="npm --registry=https://registry.npm.taobao.org \
 #   --cache=$HOME/.npm/.cache/cnpm \
 #   --disturl=https://npm.taobao.org/dist \
 #   --userconfig=$HOME/.cnpmrc"' >> ~/.zshrc && source ~/.zshrc
 
 echo '设置npm淘宝源'
-echo '\nNVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node
-\nNVM_IOJS_ORG_MIRROR=https://npm.taobao.org/mirrors/iojs' >> ~/.zshrc && source ~/.zshrc
+echo '
+NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node
+NVM_IOJS_ORG_MIRROR=https://npm.taobao.org/mirrors/iojs' >> ~/.zshrc && source ~/.zshrc
 
 # echo '安装最新node'
 # nvm install stable
